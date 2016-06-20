c05becb8dbfd77a341bd254723f105b95ad05556
loelias <loe_elias@msn.com>
2016-06-20 11:57:10 -0300

    Criação de drafts, posts antes de serem publicados.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -23,7 +24,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.autor = request.user
-            post.data_publicacao = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -44,3 +44,15 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(data_publicacao__isnull=True).order_by(
+        'data_criacao')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publica()
+    return redirect('blog.views.post_detail', pk=pk)
