37fb13735a1b748e0ce470db26215217190a4551
loelias <loe_elias@msn.com>
2016-06-20 13:04:44 -0300

    Criação da tabela comentarios no banco de dados para os posts.

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+class Comentario(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comentarios')
+    autor = models.CharField(max_length=200)
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+    comentario_aprovado = models.BooleanField(default=False)
+
+    def aprovar(self):
+        self.comentario_aprovado = True
+        self.save()
+
+    def __str__(self):
+        return self.texto
